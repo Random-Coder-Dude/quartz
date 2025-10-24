5b635e250453470536cd93641a19bee047346713
Atharv - Prog <atvkumar10@gmail.com>
2025-10-24 09:28:52 -0400

    Change page title from 'The Vault' to 'Thug Notes'

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "The Vault",
+    pageTitle: "Thug Notes",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
